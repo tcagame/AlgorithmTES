fd4cd25a902013d4a2e92eee06dfd601c391b2b1
Iwafune kohsei <@gamil.com>
2017-03-28 12:12:40 +0900

    画像を表示できるようにしました

--- AlgorithmTES/AlgorithmTES/AlgorithmTES.cpp
+++ AlgorithmTES/AlgorithmTES/AlgorithmTES.cpp
@@ -3,6 +3,8 @@
 
 void draw(int sx, int sy, int tx, int ty, int tw, int th) {
 	// dxlib‚Ì•`‰æŠÖ”‚ğ‘‚­
-	int handle = LoadGraph( "Resouce.png");
+	int handle = LoadGraph( "../Resouce.png");
 	DrawRectGraph( sx, sy, tx, ty, tw, th, handle, TRUE, FALSE );
+
 }
+
