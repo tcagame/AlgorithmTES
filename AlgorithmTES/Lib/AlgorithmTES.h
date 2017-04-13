cb42013212e96fb1ce53a94bbc810c596417c598
okuty <a>
2017-04-13 13:18:38 +0900

    Lib削除

--- AlgorithmTES/Lib/AlgorithmTES.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#pragma comment( lib, "AlgorithmTES.lib" )
-
-void draw( int sx, int sy, int tx, int ty, int tw, int th );
-bool isArrowUp( );
-bool isArrowDown( );
-bool isArrowRight( );
-bool isArrowLeft( );
